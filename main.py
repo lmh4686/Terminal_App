b68fbb3d8fc91f6f8398aac27c3451495a146689
Jihyuk Lee <lmh4686@gmail.com>
2022-09-17 15:41:01 +1000

    grain/fruit_farm() loop reinfoced.

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from random import randint, randrange, choice
 
-fruit, vegi, home, off = "Go to Fruit farm", "Go to Vegetable farm", "Go to Home", "Quit the game"
+fruit, grain, home, off = "Go to Fruit farm", "Go to Grain farm", "Go to Home", "Quit the game"
 decision_temp = "Enter a represented number to make a decision."
-fruit_msg = "You are at the Fruit farm"
-vegi_arv = "You are at the Vegetable farm"
-home_arv = "You are at the home"
+fruit_arv_msg = "You are at the Fruit farm"
+grain_arv_msg = "You are at the Grain farm"
+home_arv_msg = "You are in home"
 err_msg = "Invalid input. Type a number only."
-fruit_obj = ("Grapes", "Apple", "Orange")
-bag = {"Grapes": 0, "Apple": 0, "Orange": 0}
+fruit_obj = ("Plum", "Apple", "Orange")
+grain_obj = ("Wheat", "Oat", "Corn")
+bag = {"Plum": 0, "Apple": 0, "Orange": 0, "Wheat": 0, "Oat": 0, "Corn": 0}
+bag_limit = 30
 
 
 class InputError(Exception):
@@ -17,13 +19,15 @@ class InputError(Exception):
 
 def bag_add(item, amount):
     bag[item] += amount
-    print(f"You obtained {amount} {item}(s).\nBag : {bag}\nYou have {20 - sum(bag.values())} storage left.")
+    print(f"You obtained {amount} {item}(s).\nBag : {bag}\nYou have {bag_limit - sum(bag.values())} storage left.")
     joint_prompt()
+    if sum(bag.values()) == bag_limit:
+        pass
 
 
 def bag_full(item):
-    bag[item] = bag[item] + (20 - sum(bag.values()))
-    print(f"You obtained {20 - sum(bag.values())} {item}(s). Directing to home to empty the bag.")
+    bag[item] = bag[item] + (bag_limit - sum(bag.values()))
+    print(f"You obtained {bag_limit - sum(bag.values())} {item}(s). Directing to home to empty the bag.")
     joint_prompt()
 
 
@@ -39,7 +43,7 @@ def keyboard_int_msg(exit_num):
 def farm_choice():
     while True:
         try:
-            decision = input(f"{decision_temp}\n1.{fruit}\n2.{vegi}\n3.{off}\n")
+            decision = input(f"{decision_temp}\n1.{fruit}\n2.{grain}\n3.{off}\n")
             if decision == '1':
                 return fruit_farm()
             elif decision == '2':
@@ -67,33 +71,67 @@ def joint_prompt():
 
 
 def fruit_farm():
-    print(fruit_msg)
+    print(fruit_arv_msg)
     joint_prompt()
     while True:
-        harvested_amount = randrange(1, 9)
-        item = fruit_obj[randrange(0, len(fruit_obj) - 1)]
-        try:
-            decision = input(f"You found '{item}'(s)!!!\n{decision_temp}"
-                             f"\n(1)Harvest (2)Skip (3){vegi} (4){off} (5){home}\n")
-            if decision == '1' and sum(bag.values()) + harvested_amount <= 20:
-                bag_add(item, harvested_amount)
-            elif decision == '1' and sum(bag.values()) + harvested_amount > 20:
-                bag_full(item)
-                pass
-            elif decision == '2':
-                pass
-            elif decision == '3':
-                pass
-            elif decision == '4':
-                quit_game()
-            elif decision == '5':
-                pass
-            else:
-                raise InputError(decision)
-        except InputError:
-            print(InputError(decision))
-        except KeyboardInterrupt:
-            keyboard_int_msg(4)
+        harvested_amount = randrange(1, 3)
+        item = fruit_obj[randrange(0, len(fruit_obj))]
+        while True:
+            try:
+                decision = input(f"You found '{item}'(s)!!!\n{decision_temp}"
+                                 f"\n(1)Harvest (2)Skip (3){grain} (4){home} (5){off}\n")
+                if decision == '1' and sum(bag.values()) + harvested_amount <= bag_limit:
+                    bag_add(item, harvested_amount)
+                    break
+                elif decision == '1' and sum(bag.values()) + harvested_amount > bag_limit:
+                    bag_full(item)
+                    print("Bag full")
+                elif decision == '2':
+                    break
+                elif decision == '3':
+                    return grain_farm()
+                elif decision == '4':
+                    pass
+                elif decision == '5':
+                    return quit_game()
+                else:
+                    raise InputError(decision)
+            except InputError:
+                print(InputError(decision))
+            except KeyboardInterrupt:
+                keyboard_int_msg(5)
 
+
+def grain_farm():
+    print(grain_arv_msg)
+    joint_prompt()
+    while True:
+        harvested_amount = randrange(1, 3)
+        item = grain_obj[randrange(0, len(fruit_obj))]
+        while True:
+            try:
+                decision = input(f"You found '{item}'(s)!!!\n{decision_temp}"
+                                 f"\n(1)Harvest (2)Skip (3){fruit} (4){home} (5){off}\n")
+                if decision == '1' and sum(bag.values()) + harvested_amount <= bag_limit:
+                    bag_add(item, harvested_amount)
+                    break
+                elif decision == '1' and sum(bag.values()) + harvested_amount > bag_limit:
+                    bag_full(item)
+                    print("Bag full")
+                elif decision == '2':
+                    break
+                elif decision == '3':
+                    return fruit_farm()
+                elif decision == '4':
+                    pass
+                elif decision == '5':
+                    quit_game()
+                else:
+                    raise InputError(decision)
+            except InputError:
+                print(InputError(decision))
+            except KeyboardInterrupt:
+                keyboard_int_msg(5)
 
 farm_choice()
+
